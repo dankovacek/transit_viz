99a9bb02bacf0c8bbb0bc29f51b2575c12dd3f6d
Danbot <dankovacek@gmail.com>
2017-03-02 01:31:50 -0800

    search all 31-day periods with more than 30 one-way trips, add up the total money spent on those trips

--- README.md
+++ README.md
@@ -1,14 +1,25 @@
-# Visualization of Translink Usage
+# Calculate your Transit Tax Deduction Amount
 
 ## To use:
 
 1.  Log into your <a href="https://www.compasscard.ca/">compass account</a>
-2.  Download as much of your trip history as you want to use
-3.  Concatenate your trip history into one csv.
-    Note, don't change the column formatting or header row.
-4.  Save the concatenated csv file to the same folder as cmp_viz.py
+2.  Download all transactions from your desired tax year.  At the time of writing, Compass permitted a maximum 90 day period for downloading your usage history so you'll have to do this a few times to get the whole year.
+3.  Move all the csv files you downloaded to the '/data' folder.
 
-4.  Run the cmp_viz.py script in Python 3.5+
-5.  Share your ridership vizualization!  @postnostills
-6.  If you're really keen, improve the code (it's really bad)
-    or add functionality.
+The filter on the compass website might still be a bit wonky, so don't worry if you get some duplicate dates, or entries with dates outside the tax year you want.  These anomalies are taken care of.
+
+4.  From command line, run the cmp_viz.py script in Python 3.5+
+    (<a href="https://www.python.org/about/gettingstarted/">What's Python?  How do I get it?</a>)
+
+5.  The printed output should tell you your eligible transit usage date range, and the total value spent in that time period, which *if the code is correct* corresponds to the Deduction Amount.
+
+6.  If you're really keen, improve the code (it's really bad, yeah.), documentation, or functionality.
+
+7.  If you're a hero to all those good folks who do public service by using transit and not contributing to traffic, make it into a publicly available, free web app!
+
+## License & Disclaimer
+The author does not guarantee accuracy of the information provided.  Use at your own peril.
+
+## License
+
+<a href="https://creativecommons.org/licenses/by-nc/2.0/ca/">Attribution-NonCommercial 2.0 Canada (CC BY-NC 2.0 CA)</a>
--- cmp_viz.py
+++ cmp_viz.py
@@ -12,33 +12,16 @@ import re
 import json
 
 # Import ordinary least squares from statsmodels
-import statsmodels.api as sm
-from statsmodels.formula.api import ols
-import scipy.stats as stats
-import matplotlib.pyplot as plt
-from matplotlib import rcParams
 import pandas as pd
 import numpy as np
 
-import bokeh
-from bokeh.io import hplot, gridplot
-from bokeh.layouts import gridplot
-from bokeh.plotting import figure, output_file, show, ColumnDataSource
-from bokeh.models import HoverTool, BoxSelectTool, ResetTool, SaveTool
-from bokeh.models import BoxZoomTool, LassoSelectTool, BoxSelectTool, PanTool
-from bokeh.models import ResizeTool, UndoTool, RedoTool, BoxAnnotation
-from bokeh.models import DatetimeTickFormatter, Legend
-from bokeh.models import (
-  GMapPlot, GMapOptions, Circle, DataRange1d,  WheelZoomTool)
-from bokeh.charts import TimeSeries
-from bokeh.palettes import Spectral6
-from bokeh.models import GeoJSONDataSource
-from bokeh.tile_providers import STAMEN_TONER
-from bokeh.tile_providers import CARTODBPOSITRON_RETINA
-from bokeh.sampledata.sample_geojson import geojson
-from bokeh.models.markers import Circle
-
 DATA_PATH = os.path.dirname(os.path.abspath(__file__))
+
+#########################
+#  SET TAX YEAR
+TAX_YEAR = 2016
+#########################
+
 
 def load_trip_summary(file_list):
     """
@@ -49,193 +32,112 @@ def load_trip_summary(file_list):
     card_action = []
     trip_dataframe = pd.DataFrame()
 
+    # create a date list to avoid duplicate entries
+    all_datetimes = []
+
     for file_path in file_list:
         with open(file_path, 'rU') as csvfile:
             # Note that the example csv was TAB DELIMITED
             compass_file = csv.reader(csvfile)
             for row in compass_file:
                 # A proper implementation of checking the header format
-                # will be needed.  This is a temporary solution and a bad idea.
+                # will be needed.  This is a bad hack.
                 if row[0] == 'DateTime':
                     headers = row
                 else:
                     datetime = pd.to_datetime(row[0])
-                    print(datetime)
-                    date_str = row[0]
-                    location = row[1].upper()
-                    location = location.replace('STREET', 'ST')
-                    location = location.replace(' ', '')[:-3]
-                    transaction = row[2]
-                    product = row[3]
-                    amount = float(row[4])
+                    # avoid adding duplicate entries because Compass export
+                    # date range doesn't work.  Also make sure only trips
+                    # in the desired tax year are included.
+                    extraneous_entries = ['Missing', 'Transfer']
+                    if row[0] not in all_datetimes and datetime.year == TAX_YEAR:
+                        all_datetimes += [row[0]]
+                        # filter out missing taps and transfers as they
+                        # are interested in just the tap in - tap out combo
+                        # that makes up the one-way trip we're targeting.
+                        excluded_entry = False
+                        for e in extraneous_entries:
+                            if e in row[2].split():
+                                excluded_entry = True
+                        if excluded_entry == False:
+                            location = row[1]
+                            transaction = row[2]
+                            product = row[3]
+                            amount = float(row[4])
+                            card_action += [[datetime, location, transaction, product, amount]]
 
-                    card_action += [[datetime, date_str, location, transaction, product, amount]]
+    return pd.DataFrame(card_action, columns=headers)
 
-    return pd.DataFrame(q_msmts, columns=headers)
-
-
-import xml.etree.ElementTree as etree
-
-def deg2num(lat_deg, lon_deg, zoom):
-    '''
-    Converts latlon decimal degrees to WMTS format:
-    http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Lon..2Flat._to_tile_numbers_2
-    '''
-    lat_rad = math.radians(lat_deg)
-    n = 2.0 ** zoom
-    xtile = int((lon_deg + 180.0) / 360.0 * n)
-    ytile = int((1.0 - math.log(math.tan(lat_rad) + (1 / math.cos(lat_rad))) / math.pi) / 2.0 * n)
-    return (xtile, ytile)
-
-def parse_kml(f):
-    """ Takes in as input the Translink stations kml file
-    downloaded from the Vancouver Open Data Catalogue:
-    http://data.vancouver.ca/datacatalogue/
-    and returns the station name and coordinates in
-    a pandas dataframe.  Note that only stations
-    in the city limits are included."""
-
-    tree = etree.parse(DATA_PATH + '/' + f)
-
-    # retrieve header information from file
-    # If the device setup changes, make adjustments to tags here.
-    stations = []
-    lat = []
-    lon = []
-
-    i = 0
-    for elem in tree.iter():
-        # remove the url prefix on the element tag
-        tag = elem.tag.split('}')[-1]
-
-        # we're only interested in the name and coordinates
-        if tag == 'name':
-            # a bunch of useless crap to try matching kml location format
-            formatted_name = elem.text
-            formatted_name = formatted_name.replace('.', '')
-            formatted_name = formatted_name.replace('\\n', '')
-            formatted_name = formatted_name.replace(' ', '').upper()
-            stations += [formatted_name]#.split('-')
-        elif tag == 'coordinates':
-            lat_lon = elem.text.split(',')[:2]
-            temp_lat = float(lat_lon[1].strip())
-            temp_lon = float(lat_lon[0].strip())
-            lat += [temp_lat]
-            lon += [temp_lon]
-
-    # create a new dataframe
-    station_df = pd.DataFrame()
-
-    # create column titles for the dataframe
-    header = ['station_name', 'lat', 'lon']
-
-    # add the station array to the dataframe
-    # the first two entries are not stations, so eliminate them
-    station_df[header[0]] = stations[2:]
-
-    # add the coordinates array to the dataframe
-    station_df[header[1]] = lat
-    station_df[header[2]] = lon
-
-    return pd.DataFrame(station_df, columns=header)
 
 file_list = glob.glob(DATA_PATH + '/data/*.csv')
 # load the discharge summary into a pandas dataframe
 trips_df = load_trip_summary(file_list)
 
-# parse the kml file of translink stations within CoV limits
-station_kml = 'rapid_transit_stations.kml'
-stations_df = parse_kml(station_kml)
+headers = trips_df.columns.values
+# headers[0] should be datetime
+trips_df = trips_df.sort_values(by='DateTime')
 
-# output the station list with coordinates to csv
-# and unfortunately manually correct the names to match
-# the compass output names
-# filename = 'CoV_Translink_Stns_fmtd'
-# filename += datetime.date.today().strftime("%Y%B%d") + '.csv'
-# stations_df.to_csv(filename)
+# according to CRA, to claim your Compass transit amount,
+# your card must be used to make a minimum
+# 32 one-way trips over a maximum 31 consecutive days
+# http://www.cra-arc.gc.ca/tx/ndvdls/tpcs/ncm-tx/rtrn/cmpltng/ddctns/lns360-390/364/lgblty-eng.html
 
-# iterate through the trips and count numbers of trips
-locations = {}
-for e in trips_df['Location'].iteritems():
-    if e[1] not in locations.keys():
-        locations[e[1]] = 1
-    else:
-        locations[e[1]] += 1
+min_trips = 28
+min_days = pd.Timedelta('31 days')
 
-# determine the total number of trips
-total_trips = sum([locations[e] for e in locations.keys()])
+first_day = min(trips_df['DateTime'])
+last_day = max(trips_df['DateTime'])
 
-# Find all 31 day periods with minimum 32 one-way trips
-# "the card is used to make at least 32 one-way
-# trips over a maximum of 31 consecutive days"
+# create an array to track eligible one-way trips
+eligible_trips = []
+
+most_trips = 0
+
+all_eligible_trips = pd.DataFrame()
+
 for i in range(len(trips_df['DateTime'])):
-    print(trips_df.ix[i])
+    curr_date = trips_df.ix[i][0]
+    end_date = pd.to_datetime(curr_date + min_days)
 
-# START Trip Visualization
+    # filter for all datetimes in the given 31-day period
+    mask = (trips_df['DateTime'] > curr_date) & (trips_df['DateTime'] < end_date)
+    date_slice = trips_df.loc[mask]
 
-# normalized_trips = [(e, float(locations[e] / total_trips)) for e in locations.keys()]
-# #print('normalized trips: ', normalized_trips)
-# #now put the renamed station csv back in order
-# station_list_file = 'CoV_Translink_Stns_formatted2017February21.csv'
-# stations_df = pd.DataFrame()
-# temp_places = []
-# temp_lat = []
-# temp_lon = []
-# temp_visits = []
-# with open(station_list_file, 'rU') as csvfile:
-#     for e in csvfile:
-#         temp_list = e.split(',')
-#         if temp_list[0] is not '':
-#             if temp_list[1] in locations.keys():
-#                 lt = round(float(temp_list[2].strip()), 5)
-#                 ln = round(float(temp_list[3].strip()), 5)
-#                 zoom = 18
-#                 # convert to WMTS
-#                 #lt1, ln1 = deg2num(lt, ln, zoom)
-#                 temp_lat += [lt]
-#                 temp_lon += [ln]
-#                 temp_places += [temp_list[1]]
-#                 temp_visits += [locations[temp_list[1]]]
+    # get an array of all the 'tap in' transactions, as these
+    # represent a unique one-way trip
+    tap_ins = [e for e in date_slice['Transaction'] if e.find('Tap in') != -1]
+    #print('tap_ins = ', tap_ins)
 
-#     # convert lat and lon to WMTS format
-#     stations_df['stations'] = temp_places
-#     stations_df['lat'] = temp_lat
-#     stations_df['lon'] = temp_lon
+    # track the highest number of trips in a 31-day period
+    if len(tap_ins) > most_trips:
+        most_trips = len(tap_ins)
 
-#     #normalize the visits so the numbers will represent a plot size
-#     #between 5 and 50
-#     normalized_visits = [float(e/total_trips) for e in temp_visits]
-#     normalized_visits = [2*math.log2(round((e/5)*10**4, 0)) for e in normalized_visits]
-#     stations_df['size'] = normalized_visits
+    # if there are more than the minimum required trips
+    # in the 31-day period, add the trips to the all_eligible_trips
+    # dataframe
+    if len(tap_ins) >= min_trips:
+        start_day = min(date_slice['DateTime'])
+        end_day = max(date_slice['DateTime'])
 
-# source = ColumnDataSource(
-#     data=dict(
-#         name=stations_df['stations'],
-#         lat=stations_df['lat'],
-#         lon=stations_df['lon'],
-#         size=stations_df['size'],
-#         )
-#     )
+        # if one eligible period has been found
+        # and we find another, check if the start date
+        # of the latest eligible period falls within the
+        # range already registered.  If so, add only the
+        # additional dates (union, no duplicate entries).
+        # If not, add the whole period,
+        if not all_eligible_trips.empty:
+            if start_day in all_eligible_trips['DateTime']:
+                # merge the two dataframes
+                all_eligible_trips = pd.merge(all_eligible_trips, date_slice, \
+                    how='outer', on='DateTime')
+        else:
+            all_eligible_trips = date_slice
 
-# x_mean = np.mean([e for e in stations_df['lat']])
-# y_mean = np.mean([e for e in stations_df['lon']])
-# # map the data
-# map_options = GMapOptions(lat=x_mean, lng=y_mean, map_type="roadmap", zoom=11)
+# Add up the transaction values in the resultant overall eligible
+# trip period
+total_transaction_value = abs(sum([v for v in all_eligible_trips['Amount']]))
 
-# plot = GMapPlot(
-#     x_range=DataRange1d(), y_range=DataRange1d(), map_options=map_options
-# )
-
-# # keep the api key
-# import client_secrets
-# # replace 'client_secrets.send_api_key()' with your own api key in
-# # string format.  This is to keep my api key off of a public repository
-# # on github
-# apikey = client_secrets.send_api_key()
-# plot.api_key = apikey
-# #fig = figure(tools='pan, wheel_zoom')#, x_range=(x_min, x_max), y_range=(y_min, y_max))#, x_range=(-bound, bound), y_range=(-bound, bound))
-# circle = Circle(x='lon', y='lat', size='size', fill_alpha=0.8, fill_color='#ff8856', line_color='#ff8856')
-# plot.add_glyph(source, circle)
-# plot.add_tools(PanTool(), WheelZoomTool())
-# show(plot)
+print('eligible Compass Usage Period = ', min(all_eligible_trips['DateTime']), \
+    ' to ', max(all_eligible_trips['DateTime']))
+print('total transaction value = $', total_transaction_value )
 
